3835ea946ffbe4162b61b1e0fb0366ce26904dbe
Heesu Kim <hkim0058@student.monash.edu>
2026-06-18 14:01:36 +0800

    initialize spring boot project with core directory layout

---
+++ src/main/java/me/heesukim/mypasswordmanager/MypasswordmanagerApplication.java
@@ -0,0 +1,13 @@
+package me.heesukim.mypasswordmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypasswordmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypasswordmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/heesukim/mypasswordmanager/MypasswordmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.heesukim.mypasswordmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypasswordmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
